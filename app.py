504ba47ef42055dda4683b81dd84d242c236265f
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-02-08 15:11:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ import os
 
 # Function to load data based on selected state, district, and taluka
 def load_data(state, district, taluka):
-    file_path = os.path.join("population", state, district )
+    file_path = os.path.join(state, district + ".xlsx")
     df = pd.read_excel(file_path, sheet_name=taluka)
     return df
 
 # Main function to run the Streamlit app
 def main():
-    st.title("Population Data Viewer")
+    st.title("Population Data Viewer For Genericart")
 
     # Get list of states
-    states = os.listdir("population")
+    states = os.listdir()
 
     # Dropdown to select state
     selected_state = st.selectbox("Select State", states)
 
     # Get list of districts in selected state
-    districts = os.listdir(os.path.join("population", selected_state))
+    districts = os.listdir(selected_state)
 
     # Dropdown to select district
     selected_district = st.selectbox("Select District", districts)
 
     # Get list of talukas in selected district
-    talukas = pd.ExcelFile(os.path.join("population", selected_state, selected_district )).sheet_names
+    talukas = pd.ExcelFile(os.path.join(selected_state, selected_district + ".xlsx")).sheet_names
 
     # Dropdown to select taluka
     selected_taluka = st.selectbox("Select Taluka", talukas)
